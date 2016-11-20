c582316eab939f01914499bd5a4615c5d7c96d89
root <nicolasrich@hotmail.fr>
2016-11-20 14:19:31 +0100

    Tests combinaison

---
+++ src/php/view/Test.php
@@ -0,0 +1,26 @@
+<?php
+// On teste les fonctionnalités apportées
+require_once "../model/Partie.php";
+
+/*
+Couleurs : OK
+$c1 = new Couleur(1);
+$c2 = new Couleur(2);
+$c3 = new Couleur(3);
+$c4 = new Couleur(4);
+$c5 = new Couleur(5);
+$c6 = new Couleur(6);
+$c7 = new Couleur(7);
+$c8 = new Couleur(8);
+
+Combinaisons : OK
+$combi1 = new Combinaison($c2,$c2,$c3,$c4);
+$combi1->testAfficher();
+echo '</br>';
+$combi2 = new Combinaison($c3,$c4,$c2,$c4);
+$combi2->testAfficher();
+
+$combi3 = $combi1->compareTo($combi2);
+$combi3->testAfficher();
+*/
+?>
