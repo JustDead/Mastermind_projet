ec79d8e5709343a76f73095c804f407e811bc421
root <nicolasrich@hotmail.fr>
2016-12-04 15:00:12 +0100

    Commentaires

--- src/php/model/BaseDonnees.php
+++ src/php/model/BaseDonnees.php
@@ -31,11 +31,8 @@ class TableAccesException extends MonException{
 class BaseDonnees{
 private $connexion;
 
-// Constructeur de la classe
-// remplacer X par les informations qui vous concernent
-
   public function __construct(){
-   try{
+   try{ //On se connecte à notre base avec notre identifiant
       $chaine="mysql:host=localhost;dbname=E155059S";
       $this->connexion = new PDO($chaine,"E155059S","E155059S");
       $this->connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -47,44 +44,15 @@ private $connexion;
   }
 
 
-
-
-// A développer
 // méthode qui permet de se deconnecter de la base
 public function deconnexion(){
    $this->connexion=null;
 }
 
 
-//A développer
-// utiliser une requête classique
-// méthode qui permet de récupérer les pseudos dans la table pseudo
-// post-condition:
-//retourne un tableau à une dimension qui contient les pseudos.
-// si un problème est rencontré, une exception de type TableAccesException est levée
 
-public function getPseudos(){
- try{
+// Méthode qui indique si un pseudo est présent dans la table joueurs
 
-$statement=$this->connexion->query("SELECT pseudo from joueurs;");
-
-while($ligne=$statement->fetch()){
-$result[]=$ligne['pseudo'];
-}
-return($result);
-}
-catch(PDOException $e){
-    throw new TableAccesException("problème avec la table joueurs");
-  }
-}
-
-
-
-//A développer
-// utiliser une requête préparée
-//vérifie qu'un pseudo existe dans la table pseudonyme
-// post-condition retourne vrai si le pseudo existe sinon faux
-// si un problème est rencontré, une exception de type TableAccesException est levée
 public function exists($pseudo){
 try{
 	$statement = $this->connexion->prepare("select pseudo from joueurs where pseudo=?;");
@@ -106,6 +74,8 @@ catch(PDOException $e){
     }
 }
 
+// Méthode qui renvoie le mot de passe associé à un joueur
+
 public function getMdp($pseudo){
 try{
 	$statement = $this->connexion->prepare("select motDePasse from joueurs where pseudo=?;");
@@ -121,6 +91,8 @@ catch(PDOException $e){
     throw new TableAccesException("problème avec la table joueurs");
     }
 }
+
+// Méthode qui insère un tuple dans la table joueurs, après vérification de l'unicité du pseudo
 
 public function inscrireJoueur($pseudo, $psw){
   if ($this->exists($pseudo)){ // Pseudo déjà dedans
@@ -142,20 +114,25 @@ public function inscrireJoueur($pseudo, $psw){
   }
 }
 
+// Méthode qui renvoie des statistiques sur le stuples concernant un joueur donné dans la table parties
+
 function getStatsJoueur($pseudo){
   try{
+    // Nombre de partie gagnées
     $statement = $this->connexion->prepare("select sum(partieGagnee) from parties where pseudo=? and partieGagnee=1;");
     $statement->bindParam(1, $pseudoParam);
     $pseudoParam=$pseudo;
     $statement->execute();
     $result=$statement->fetch(PDO::FETCH_ASSOC);
 
+    //Nombre de parties jouées
     $statement2 = $this->connexion->prepare("select count(*) from parties where pseudo=?;");
     $statement2->bindParam(1, $pseudoParam);
     $pseudoParam=$pseudo;
     $statement2->execute();
     $result2=$statement2->fetch(PDO::FETCH_ASSOC);
 
+    // Nombre de coups totaux dans les parties gagnées
     $statement3 = $this->connexion->prepare("select sum(nombreCoups) from parties where pseudo=? and partieGagnee=1;");
     $statement3->bindParam(1, $pseudoParam);
     $pseudoParam=$pseudo;
@@ -172,14 +149,7 @@ function getStatsJoueur($pseudo){
 }
 
 
-
-
-//A développer
-// utiliser uen requête préparée
-// ajoute un message sur le salon => pseudonyme + message
-// precondition: le pseudo existe dans la table pseudonyme
-// post-condition: le message est ajouté dans la table salon
-// si un problème est rencontré, une exception de type TableAccesException est levée
+// Méthode qui insère un tuple dans la table parties à partir des données de la partie passée
 
 public function majParties($partie){
       try{
@@ -196,20 +166,9 @@ public function majParties($partie){
     }
 }
 
-
-
-
-
-
-//A développer
-//utiliser une requête classique
-// méthode qui permet de récupérer les 10 derniers messages émis sur le salon
-// post-condition:
-//retourne un tableau à deux dimensions dont le premier indice est un entier (commence à 0) qui correspond au numéro de ligne du résultat
-// c'est en fait simplement le résultat de l'application de la méthode fetchAll() avec le bon paramètre
-// le second indice est une chaine de caractère qui correspond au nom de la colonne dans la table
-// si un problème est rencontré, une exception de type TableAccesException est levée
-
+/*
+Méthode qui renvoie les 5 tuples possédant le nombre de coups le plus faible dans la table parties
+*/
 public function getHighScores(){
 
       try{
